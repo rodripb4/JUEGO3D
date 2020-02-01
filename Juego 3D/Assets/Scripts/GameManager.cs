797e0e4cd89df1e16230090aeb3690594667c905
rodripb4 <rodripb4@hotmail.com>
2020-02-01 19:59:58 +0100

    Primer Commit
    Falta musica y sombras del personaje principal

---
+++ Juego 3D/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+
+    public float restartDelay = 2f;
+
+    public GameObject completeLevelUI;
+
+
+    public void CompleteLvl()
+    {
+        completeLevelUI.SetActive(true);
+    }
+    public void EndGame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Restart();
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
